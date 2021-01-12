c754cffc807d47c5e67cdb5fec0b2549e2838af3
julieitwaru <72779833+julieitwaru@users.noreply.github.com>
2021-01-12 10:00:34 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 while True:
 
-    #if light level is greater than 15, then set neopixels of
+    #if light level is greater than 13, then set neopixels of
     print("light level:" + input.light_level())
-    if input.light_level() > 15:
+    if input.light_level() > 13:
         light.clear()
 
     #if light level is equal to or greater than 6, then set neopixels blue
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 while (true) {
-    // if light level is greater than 15, then set neopixels of
+    // if light level is greater than 13, then set neopixels of
     console.log("light level:" + input.lightLevel())
-    if (input.lightLevel() > 15) {
+    if (input.lightLevel() > 13) {
         light.clear()
     } else if (input.lightLevel() >= 6) {
         // if light level is equal to or greater than 6, then set neopixels blue
